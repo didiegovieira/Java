393043eba458309737078d4532b1cb7add48c247
didiegovieira <diego.vieira@sptech.school>
2023-03-05 23:02:47 -0300

    Arquivos de Estudo

---
+++ Projeto Individual Jar/projeto-individual/src/main/java/com/mycompany/projeto/individual/ProjetoIndividual.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto.individual;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego
+ */
+public class ProjetoIndividual {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        Integer escolhaMenu = 0;
+                
+        do {            
+            
+        } while (escolhaMenu < 0 || escolhaMenu > 6);
+    }
+}
